a1d337f7e767a76fc9afb1e839a342378619b0d3
Arco Dielhof <arcodielhof@hotmail.com>
2021-04-27 00:30:32 +0200

    Version++;

--- ManaManagement.lua
+++ ManaManagement.lua
@@ -2,7 +2,7 @@ if AZP == nil then AZP = {} end
 if AZP.VersionControl == nil then AZP.VersionControl = {} end
 if AZP.OnLoad == nil then AZP.OnLoad = {} end
 
-AZP.VersionControl["Mana Management"] = 10
+AZP.VersionControl["Mana Management"] = 11
 AZP.ManaManagement = {}
 
 local AZPMMSelfOptionPanel = nil
